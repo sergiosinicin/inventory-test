7f8cc7ea16eb4e3064f82e9ebac056ac11acb7e1
Sergey Sinicin <sergey@sinicin.lv>
2020-09-18 22:38:04 +0300

    jwt-auth added

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Package Service Providers...
          */
